31b1ee651d409d2d5f9df63ccfa4d9ec9434d376
ying shu <shuyingh0714@gmail.com>
2024-03-25 20:44:07 -0400

    Add gameEnd function

--- my-project/src/main/java/main/java/com/example/Game.java
+++ my-project/src/main/java/main/java/com/example/Game.java
@@ -80,4 +80,12 @@ public class Game {
             return true;
         }
     }
+
+    /**
+    * End game when there is a winner
+    */
+    public void gameEnd() {
+        System.out.println("Game end! The winner is" + this.currentPlayer);
+        System.exit(0);
+    }
 }
