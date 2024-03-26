dea787d1d543e4db4046dc671a6c912725682f9a
ying shu <shuyingh0714@gmail.com>
2024-03-25 21:02:19 -0400

    Delete unused param tag

--- my-project/src/main/java/main/java/com/example/Player.java
+++ my-project/src/main/java/main/java/com/example/Player.java
@@ -85,7 +85,6 @@ public class Player {
     * @param board - game board that has boundary
     * @param x - the targeted x location
     * @param y - the targeted y location
-    * @param game - current game
     * @return boolean that demonstrates whether the player move the worker successfully
     */
     public boolean moveWorker(Validator validator, Board board, int x, int y) {
