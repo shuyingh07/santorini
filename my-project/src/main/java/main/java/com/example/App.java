be87537ee6272c88d77af357cca75df1d1d57d91
ying shu <shuyingh0714@gmail.com>
2024-04-07 15:36:50 -0400

    Add api for move and build

--- my-project/src/main/java/main/java/com/example/App.java
+++ my-project/src/main/java/main/java/com/example/App.java
@@ -1,13 +1,18 @@
 package main.java.com.example;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
 import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.SimpleWebServer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
-public class App extends NanoHTTPD {
+public class App extends SimpleWebServer {
 
+    /**
+     * The main method to start the server application.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         try {
             new App();
@@ -17,154 +22,78 @@ public class App extends NanoHTTPD {
     }
 
     private Game game;
-    private Board board;
-    private Validator validator;
-    private int setup_stage = 0;
-    private static final int PORT = 8080;
 
+    /**
+     * Start the server at :8080 port.
+     */
     public App() throws IOException {
-        super(PORT);
-
-        this.game = new Game();
-        this.board = new Board(5, 5);
-
+        super(null, 8080, new File("/var/www/html"), true);
+        this.game = new Game(); // Initialize the game
         start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
         System.out.println("\nRunning!\n");
     }
 
+    /**
+     * Handles HTTP requests and returns responses.
+     * This method processes various game actions based on the request URI and parameters received in the HTTP session.
+     * Supported actions include initializing a new game, placing workers, moving workers, building structures, setting god cards, and passing actions.
+     * After processing an action, the method updates the game state and returns it in JSON format.
+     *
+     * @param session The HTTP session containing the request data.
+     * @return A fixed length response with the updated game state in JSON format.
+     */
     @Override
     public Response serve(IHTTPSession session) {
-
+        // Extract the URI and parameters from the HTTP session
         String uri = session.getUri();
         @SuppressWarnings("deprecation")
         Map<String, String> params = session.getParms();
+        GameState gameState = new GameState();
 
-        game.gameStart();
-        //this.setWorkerId = game.getCurrState().getSetWorkerId();
+        // Process the request based on the URI
 
-        if (uri.equals("/test")) {
-            System.out.println("Testing connection: server side received!");
-            String myjsonstr = "{ \"test1\": \"" + 1 + "\"," +
-                    " \"test2\": \"" + 0 + "\"," +
-                    " \"test3\": \"" + 1 + "\"}";
+        if (uri.startsWith("/api/")) {
+            System.out.println(uri);
+            switch (uri) {
+                // Initialize a new game
+                case "/api/newGame":
+                    this.game = new Game();
+                    break;
+                case "/api/initialize":
+                    // Initialize workers on the board
+                    this.game = this.game.initializeWorker(
+                            Integer.parseInt(params.get("x1")), Integer.parseInt(params.get("y1")),
+                            Integer.parseInt(params.get("x2")), Integer.parseInt(params.get("y2")));
+                    break;
+                // Move a worker on the board
+                case "/api/move":
+                    this.game = this.game.move(
+                            Integer.parseInt(params.get("x1")), Integer.parseInt(params.get("y1")),
+                            Integer.parseInt(params.get("x2")), Integer.parseInt(params.get("y2")));
+                    break;
+                // Build a tower or dome on the board
+                case "/api/build":
+                    this.game = this.game.build(
+                            Integer.parseInt(params.get("x1")), Integer.parseInt(params.get("y1")));
+                    break;
+                // Pass the current action
+                case "/api/passAction":
+                    this.game = this.game.passAction();
+                    break;
+                default:
+                    return super.serve(session);
+            }
 
-            return newFixedLengthResponse(myjsonstr);
-        } else if (uri.equals("/setup")) {
-            System.out.println("App: Setup request received!");
-            System.out.println(params.get("x"));
-            System.out.println(params.get("y"));
-            int x = Integer.parseInt(params.get("x"));
-            int y = Integer.parseInt(params.get("y"));
-            
-            if (setup_stage == 0){
-                Player currentPlayer = game.getCurrentPlayer();
-                currentPlayer.chooseWorker(0);
-                boolean init_result = currentPlayer.moveWorker(validator, board, x, y);
-                setup_stage += 1;
-            }
-            else if (setup_stage == 1){
-                Player currentPlayer = game.getCurrentPlayer();
-                currentPlayer.chooseWorker(1);
-                boolean init_result = currentPlayer.moveWorker(validator, board, x, y);
-                setup_stage += 1;
-            }
-            else if (setup_stage == 2){
-                game.changeCurrentPlayer();
-                Player currentPlayer = game.getCurrentPlayer();
-                currentPlayer.chooseWorker(0);
-                boolean init_result = currentPlayer.moveWorker(validator, board, x, y);
-                setup_stage += 1;
-            }
-            else if (setup_stage == 3){
-                game.changeCurrentPlayer();
-                Player currentPlayer = game.getCurrentPlayer();
-                currentPlayer.chooseWorker(1);
-                boolean init_result = currentPlayer.moveWorker(validator, board, x, y);
-            }
-            // if (state == null) {
-            //     return newFixedLengthResponse(new State(game.getCurrState(), -1).toString());
-            // }
-
-            // return newFixedLengthResponse(state.toString());
+            gameState.updateFromGame(this.game);
+            System.out.println(gameState.toJson());
+            Response response = newFixedLengthResponse(Response.Status.OK, "application/json", gameState.toJson());
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            return response;
         }
-        // } else if (uri.equals("/choosemove")) {
-        //     System.out.println("App: chooseMove request received!");
-        //     System.out.println(params.get("x"));
-        //     System.out.println(params.get("y"));
 
-        //     int x = Integer.valueOf(params.get("x"));
-        //     int y = Integer.valueOf(params.get("y"));
+        return super.serve(session);
+    }
 
-        //     State state = game.chooseMove(new Pos(x, y));
-        //     setWorkerId = game.getChosenWorkerId();
-
-        //     if (state == null) {
-        //         return newFixedLengthResponse(new State(game.getCurrState(), State.STERR).toString());
-        //     }
-        //     return newFixedLengthResponse(state.toString());
-        // } else if (uri.equals("/move")) {
-        //     System.out.println("App: move request received!");
-        //     System.out.println(params.get("x"));
-        //     System.out.println(params.get("y"));
-
-        //     int x = Integer.valueOf(params.get("x"));
-        //     int y = Integer.valueOf(params.get("y"));
-        //     int playerId = game.getNextPlayerId();
-
-        //     State state = game.move(playerId, setWorkerId, new Pos(x, y));
-        //     if (state == null) {
-        //         return newFixedLengthResponse(new State(game.getCurrState(), State.STERR).toString());
-        //     }
-        //     return newFixedLengthResponse(state.toString());
-        // } else if (uri.equals("/build")) {
-        //     System.out.println("App: Build request received!");
-        //     System.out.println(params.get("x"));
-        //     System.out.println(params.get("y"));
-
-        //     int x = Integer.valueOf(params.get("x"));
-        //     int y = Integer.valueOf(params.get("y"));
-        //     int playerId = game.getNextPlayerId();
-
-        //     State state = game.build(playerId, setWorkerId, new Pos(x, y));
-        //     if (state == null) {
-        //         return newFixedLengthResponse(new State(game.getCurrState(), State.STERR).toString());
-        //     }
-        //     return newFixedLengthResponse(state.toString());
-        // } else if (uri.equals("/newgame")) {
-        //     this.inGodMode = false;
-        //     System.out.println("App: New Game request received!");
-
-        //     this.game = new GameImpl();
-        //     setWorkerId = 1;
-
-        //     return null;
-        // } else if (uri.equals("/newgodgame")) {
-        //     this.inGodMode = true;
-        //     System.out.println("App: New God Game request received!");
-        //     String god1 = params.get("god1");
-        //     String god2 = params.get("god2");
-
-        //     this.game = new GodGame(new GameImpl(), god1, god2);
-        //     setWorkerId = 1;
-
-        //     return null;
-        // } else if (uri.equals("/preparepower1")) {
-        //     System.out.println("App: God Game player 1 use power request received!");
-        //     State state = game.setupPower(1, setWorkerId);
-        //     return newFixedLengthResponse(state.toString());
-        // } else if (uri.equals("/preparepower2")) {
-        //     System.out.println("App: God Game player 2 use power request received!");
-        //     State state = game.setupPower(2, setWorkerId);
-        //     return newFixedLengthResponse(state.toString());
-        // } else if (uri.equals("/usepower")) {
-        //     System.out.println("App: Use Power Action request received!");
-        //     System.out.println(params.get("x"));
-        //     System.out.println(params.get("y"));
-
-        //     int x = Integer.valueOf(params.get("x"));
-        //     int y = Integer.valueOf(params.get("y"));
-        //     int playerId = game.getNextPlayerId();
-
-        //     State state = game.usePower(playerId, setWorkerId, new Pos(x, y));
-        //     if (state == null) {
-        //     
+}
