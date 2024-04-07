2acf9d62a3ee0128c9236760e7d3831e284c6fc7
ying shu <shuyingh0714@gmail.com>
2024-04-07 15:39:47 -0400

    Remove winning check, change to Game.java

--- my-project/src/main/java/main/java/com/example/Player.java
+++ my-project/src/main/java/main/java/com/example/Player.java
@@ -1,127 +1,189 @@
 package main.java.com.example;
 
-import java.util.ArrayList;
 
+/**
+ * Represents a player in the game.
+ * This class encapsulates all the properties and actions of a player, including their unique identifier, status, and strategies influenced by god cards.
+ * It handles key gameplay actions like initializing workers, moving them on the board, building structures, and determining win conditions.
+ * The class also maintains the player's current status in the game and interacts with the game board to execute player actions.
+ */
 public class Player {
-    private int playerID;
-    private ArrayList<Worker> workerList;
-    private Worker currentWorker;
+    private final int playerId;  // The unique identifier for the player.
 
-    public Player(int playerID) {
-        this.playerID = playerID;
-        this.currentWorker = null;
-        this.workerList = new ArrayList<>();
-        this.workerList.add(new Worker(0));
-        this.workerList.add(new Worker(1));
+    private String status = "initialize";  // The current status of the player, indicating the phase of their turn (e.g., 'initialize', 'move', 'build').
+
+    private final Validator validator = new Validator(); // Default validation strategy for initialization, building and moving.
+
+    private boolean isExecuted = false;  // Indicates whether a specific action (like a build) has been executed in the current turn.
+
+    /**
+     * Constructor for the Player class.
+     * Initializes a new player with a unique identifier.
+     * This ID is used to distinguish the player throughout the game.
+     *
+     * @param playerId The unique identifier for the player.
+     */
+
+    public Player(int playerId) {
+        this.playerId = playerId;
     }
 
     /**
-    * Initialize two workers
-    * @param validator - check whether the location that player choose is validated
-    * @param board - game board that has boundary
-    * @param x1 - the x location of the worker1
-    * @param y1 - the y location of the worker1
-    * @param x2 - the x location of the worker2
-    * @param y2 - the y location of the worker2
-    * @return boolean that demonstrates whether the player intializes workers successfuly
-    */
-    public boolean workerInit(Validator validator, Board board, int x1, int y1, int x2, int y2) {
-        if(x1 == x2 && y1 ==y2){
+     * First, it allows player to choose the initial position for two workers
+     * Second, check if the position is valid and status is "initialize". If not, it will return false and end.
+     * If it is valid, place the two workers.
+     * @param board board of the current game
+     * @param x1 position x of worker one
+     * @param y1 position y of worker two
+     * @param x2 position x of worker two
+     * @param y2 position y of worker two
+     * @return {@code true} if successfully initialized the two workers
+     */
+    public boolean initializeWorker(Board board, int x1, int y1, int x2, int y2) {
+        // Check if the current status is 'initialize' and the first position is valid
+        // Validate the second position and ensure it's not the same as the first
+        if (status.equals("initialize") && validator.isValidInitial(board, x1, y1) && validator.isValidInitial(board, x2, y2) && !(x1 == x2 && y1 == y2)) {
+            // Update status to 'move' and initialize the two workers
+            this.status = "move";
+            board.initializeWorker(x1, y1, x2, y2, playerId);
+            return true;
+        } else {
+            // Output error message if the first initialization is invalid
+            System.out.println("Invalid initialization. Try again.");
             return false;
         }
-        
-        if(!(validator.isInBoundary(board, x1, y1))){
+    }
+
+    /**
+     * First, it allows player to choose the worker to move according to the position.
+     * Second, it allows player to choose the desired position to move.
+     * Third, it will check if this player has god card that implements move strategy
+     * if true, validation obey the god card rule will be executed, otherwise, the default validation will be executed.
+     * (The extra action for god card like pushing other workers will be implemented directly in god card validator)
+     * Forth, without god card, it will check if the steps above is valid and if the status is "move". If not, return false and end.
+     * With god card allows second move, it will check if this god card allows second move.
+     * If it is valid, move the worker to the desired place.
+     *
+     * @param board board of the current game
+     * @param fromX position x of the original worker
+     * @param fromY position y of the original worker
+     * @param toX position x of the moved worker
+     * @param toY position y of the moved worker
+     * @return {@code true} if the worker is moved successfully
+     */
+    public boolean move(Board board, int fromX, int fromY, int toX, int toY) {
+        // Check the validity of the move, either through the standard rules or a specific move strategy
+        if ((status.equals("move") && validator.isValidMove(board, fromX, fromY, toX, toY, playerId))) {
+            // Update the worker's position on the board and change the status to 'build'
+            board.findAndPlaceWorker(fromX, fromY, toX, toY, playerId);
+            this.status = "build";
+            return true;
+        } else {
+            // Output error message if the move is invalid
+            System.out.println("Invalid move. Try again.");
             return false;
         }
+    }
 
-        if(!(validator.isInBoundary(board, x2, y2))){
+    /**
+     * First, it allows player to choose the position to build a block or dome.
+     * Second, it will check if this player has god card that implements build strategy
+     * if true, validation obey the god card rule will be executed,
+     * if false, the default validation will be executed.
+     * Third, it will check if the position is valid and the status is "build". If not, return false.
+     * Forth, without god card, it will build a block or dome.
+     * With god card allows second build, it will check if this god card allows second build.
+     * If true, check if this player has built once or not to decide if the status needs to be changed to 'move', then build a block or dome.
+     * if not, change the status to 'move' and build a block or dome.
+     * @param board board of the current game
+     * @param buildX the X position required to build a block or dome
+     * @param buildY the Y position required to build a block or dome
+     * @return {@code true} if the block or dome is successfully built
+     */
+    public boolean build(Board board, int buildX, int buildY) {
+        // Check if the build is valid under standard rules or a specific build strategy
+        if((status.equals("build") && validator.isValidBuild(board, buildX, buildY))) {
+            // Execute the build action and update the status
+            this.status = "move";
+            board.buildBlock(buildX, buildY);
+            resetIsMoved(board);
+            return true;
+        } else {
+            // Output error message if the build action is invalid
+            System.out.println("Invalid build. Try again.");
             return false;
         }
-        Grid grid1 = board.getGrid(x1, y1);
-        Grid grid2 = board.getGrid(x2, y2);
-        if(grid1.getOccupy() || grid2.getOccupy()){
-            return false;
+    }
+
+    /**
+     * Determines if a move results in a winning condition.
+     * This method checks for standard winning conditions and any additional conditions specified by a win strategy.
+     * If any worker has moved to a position meeting the winning criteria, or if the win strategy's conditions are met, the move is considered a winning move.
+     *
+     * @param board The game board on which the move is being made.
+     * @param fromX The original X coordinate of the worker.
+     * @param fromY The original Y coordinate of the worker.
+     * @param toX The target X coordinate for the worker.
+     * @param toY The target Y coordinate for the worker.
+     * @return {@code true} if the move results in a win, otherwise {@code false}.
+     */
+    public boolean isWinningMove(Board board, int fromX, int fromY, int toX, int toY) {
+        boolean isWin = validator.isWinningMove(board);
+
+        return isWin;
+    }
+
+    /**
+     * Resets the movement state of all workers on the board.
+     * This method iterates through all workers on the board and sets their 'isMoved' state to false.
+     * It is used to reset the state at the end of a turn or after a specific action has been completed,
+     * ensuring that the movement state of each worker is accurately reflected for the next turn.
+     *
+     * @param board The game board whose workers' movement states are to be reset.
+     */
+    public void resetIsMoved(Board board) {
+        // Iterate through all workers on the board
+        for(Worker worker : board.getWorkers()) {
+            // Reset the 'isMoved' state to false if it's currently true
+            if(worker.getIsMoved()) {
+                worker.setIsMoved(false);
+            }
         }
+    }
 
-        Worker worker1 = workerList.get(0);
-        worker1.move(x1, y1);
-        grid1.setOccupy();
-
-        Worker worker2 = workerList.get(1);
-        worker2.move(x2, y2);
-        grid2.setOccupy();
-        return true;
-
+    /**
+     * Retrieves the status of player.
+     * @return The status of player.
+     */
+    public String getStatus() {
+        return status;
     }
     
     /**
-    * Choose worker to do operations in this turn
-    * @param workerID - the worker that need to operate this turn
-    * @return boolean that demonstrates whether the player choose the worker successfully
-    */
-    public boolean chooseWorker(int workerID) {
-        if(workerID < 0 || workerID >= workerList.size()){
-            return false;
-        }
-
-        else{
-            Worker worker = workerList.get(workerID);
-            this.currentWorker = worker;
-            return true;
-        }
+     * Checks if a specific action has been executed by the player.
+     * @return {@code true} if the action has been executed, otherwise {@code false}.
+     */
+    public boolean getIsExecuted() {
+        return isExecuted;
     }
 
     /**
-    * Get the chosen worker in this turn
-    * @return worker that has been chosen this turn
-    */
-    public Worker getCurrentWorker() {
-        return this.currentWorker;
-    }
-
-     /**
-    * Move the chosen worker to a specified location
-    * @param validator - check whether the location that player choose is validated
-    * @param board - game board that has boundary
-    * @param x - the targeted x location
-    * @param y - the targeted y location
-    * @return boolean that demonstrates whether the player move the worker successfully
-    */
-    public boolean moveWorker(Validator validator, Board board, int x, int y) {
-        if(!(validator.isValidMove(board, currentWorker, x, y))) {
-            return false;
-        }
-
-        else{
-            Grid workeGrid = board.getGrid(currentWorker.getX(), currentWorker.getY());
-            Grid targetGrid = board.getGrid(x, y);
-            workeGrid.clearOccupy();
-            targetGrid.setOccupy();
-            this.currentWorker.move(x, y);
-
-            if(validator.isWin(board, currentWorker)){
-                System.out.println("You win the game");
-                System.exit(0);
-            }
-            return true;
-        }
+     * Sets the current status of the player.
+     * This status represents the current phase or action the player is in.
+     *
+     * @param status The status to be set for the player.
+     */
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     /**
-    * Build brick in a specified location
-    * @param validator - check whether the location that player choose is validated
-    * @param board - game board that has boundary
-    * @param x - the targeted x location
-    * @param y - the targeted y location
-    * @return boolean that demonstrates whether the player successfully build
-    */
-    public boolean build(Validator validator, Board board, int x, int y){
-        if(!(validator.isValidBuild(board, currentWorker, x, y))) {
-            return false;
-        }
-
-        else{
-            return this.currentWorker.build(board, x, y);
-        }
+     * Sets the executed state for a specific action.
+     * This is used to track whether a certain action has been performed.
+     *
+     * @param executed The executed state to be set.
+     */
+    public void setExecuted(boolean executed) {
+        isExecuted = executed;
     }
 }
--- my-project/src/main/java/main/java/com/example/Validator.java
+++ my-project/src/main/java/main/java/com/example/Validator.java
@@ -1,127 +1,117 @@
 package main.java.com.example;
 
-
+/**
+ * Provides default strategy for validating various actions in the game.
+ * This class contains methods to ensure that actions such as initializing a worker, moving a worker, and building on the board comply with the game's rules.
+ */
 public class Validator {
 
-    private static final int MAX_HEIGHT = 4;
-
-    public Validator(){
-    
+    /**
+     * Check if the initial place is within the board boundaries, the cell is unoccupied
+     * @param board board of the current game
+     * @param toX initial position x of worker
+     * @param toY initial position y of worker
+     * @return {@code true} if the position is valid to place the worker
+     */
+    public boolean isValidInitial(Board board, int toX, int toY) {
+        return isWithinBounds(toX, toY) && isCellFree(board, toX, toY);
     }
 
     /**
-    * Check whether a location is in the boundary of the board
-    * @param board - game board that has boundary
-    * @param x - specified x location
-    * @param y - specified y location
-    * @return boolean that demonstrates whether the specified location is in the boundary
-    */
-    public boolean isInBoundary(Board board, int x, int y) {
-        if(x < board.getRowLen() && y < board.getColLen() && x >= 0 && y >= 0) {
-            return true;
+     * Check if the moving position is within the board boundaries, the cell is unoccupied, the height is valid, etc.
+     * @param board board of the current game
+     * @param fromX position x of the original worker
+     * @param fromY position y of the original worker
+     * @param toX position x of the moved worker
+     * @param toY position y of the moved worker
+     * @param playerId the player id of current player
+     * @return {@code true} if the position is valid to move the worker from the original position
+     */
+    public boolean isValidMove(Board board, int fromX, int fromY, int toX, int toY, int playerId) {
+        boolean workerBelongPlayer = false;
+        for (Worker worker : board.getWorkers()) {
+            if (fromX == worker.getX() && fromY == worker.getY() && playerId == worker.getPlayerId()) {
+                workerBelongPlayer = true;
+                break;
+            }
         }
-        else{
-            return false;
-        }
+
+        return isWithinBounds(toX, toY)
+                && workerBelongPlayer
+                && board.getTowerHeight(toX, toY) <= 3 && isCellFree(board, toX, toY)
+                && Math.abs(board.getTowerHeight(toX, toY) - board.getTowerHeight(fromX, fromY)) <= 1
+                && Math.abs(fromX - toX) <= 1 && Math.abs(fromY - toY) <= 1;
     }
 
     /**
-    * Check whether a location is adjacent to a worker
-    * @param worker - the worker that player chooses this turn
-    * @param x - specified x location
-    * @param y - specified y location
-    * @return boolean that demonstrates whether the specified location is adjacent to the worker
-    */
-    public boolean isAdjacentGrid(Worker worker, int x, int y) {
-        int workerX = worker.getX();
-        int workerY = worker.getY();
+     * Check if the build position is within the board boundaries, the height is less than 4, the cell neighbored with moved worker, etc.
+     * @param board board of the current game
+     * @param x position x of the block or dome
+     * @param y position y of the block or dome
+     * @return {@code true} if the position is valid to build block or dome
+     */
+    public boolean isValidBuild(Board board, int x, int y) {
+        int workerX = -1;
+        int workerY = -1;
+        boolean hasMovedWorker = false;
+        for(Worker worker : board.getWorkers()) {
+            if(worker.getIsMoved()) {
+                workerX = worker.getX();
+                workerY = worker.getY();
+                hasMovedWorker = true;
+                break;
+            }
+        }
 
-        if((Math.abs(workerX-x) <= 1) && (Math.abs(workerY-y) <= 1)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return isWithinBounds(x, y) && board.getTowerHeight(x, y) < 4 &&
+                Math.abs(x - workerX) <= 1 && Math.abs(y - workerY) <= 1 &&
+                isCellFree(board, x, y) && hasMovedWorker &&
+                Math.abs(board.getTowerHeight(x, y) - board.getTowerHeight(workerX, workerY)) <= 1;
     }
 
     /**
-    * Check whether a worker can move successfully
-    * @param board - game board that has boundary
-    * @param worker - the worker that player chooses this turn
-    * @param x - specified x location
-    * @param y - specified y location
-    * @return boolean that demonstrates whether the worker can move to a specified location successfully
-    */
-    public boolean isValidMove(Board board, Worker worker, int x, int y) {
-        if(!isInBoundary(board, x, y)){
-            return false;
-        }
+     * Determines if a move results in a winning condition.
+     * This method checks for default winning conditions (reaching a level 3 tower on the board).
+     * If any worker belongs to current player has moved to a position meeting the winning criteria, the move is considered a winning move.
+     *
+     * @param board The game board on which the move is being made.
+     * @return {@code true} if the move results in a win, otherwise {@code false}.
+     */
+    public boolean isWinningMove(Board board) {
+        boolean ifWin = false;
 
-        if(!isAdjacentGrid(worker, x, y)){
-            return false;
+        // Check standard winning condition: any worker reaching the third level
+        for (Worker worker : board.getWorkers()) {
+            if (worker.getIsMoved() && board.getTowerHeight(worker.getX(), worker.getY()) == 3) {
+                ifWin = true;
+                break;
+            }
         }
+        return ifWin;
+    }
 
-        Grid targetGrid = board.getGrid(x, y);
-        int workerX = worker.getX();
-        int workerY = worker.getY();
-        Grid workeGrid = board.getGrid(workerX, workerY);
-        if(targetGrid.getOccupy()){
-            return false;
-        }
-        if(targetGrid.getHeight() >= MAX_HEIGHT){
-            return false;
-        }
-        if((targetGrid.getHeight() - workeGrid.getHeight()) > 1){
-            return false;
+    /**
+     * Check if the cell is within boundary
+     * @param x position x of the checked cell
+     * @param y position y of the checked cell
+     * @return {@code true} if the position within boundary
+     */
+    private boolean isWithinBounds(int x, int y) {
+        return x >= 0 && x < 5 && y >= 0 && y < 5;
+    }
+
+    /**
+     * Check if the cell is free
+     * @param x position x of the checked cell
+     * @param y position y of the checked cell
+     * @return {@code true} if the position is free
+     */
+    private boolean isCellFree(Board board, int x, int y) {
+        for (Worker worker : board.getWorkers()) {
+            if (worker != null && worker.getX() == x && worker.getY() == y) {
+                return false;
+            }
         }
         return true;
     }
-
-    /**
-    * Check whether a worker can build successfully
-    * @param board - game board that has boundary
-    * @param worker - the worker that player chooses this turn
-    * @param x - specified x location
-    * @param y - specified y location
-    * @return boolean that demonstrates whether the worker can build in a specified location successfully
-    */
-    public boolean isValidBuild(Board board, Worker worker, int x, int y) {
-        if(!isInBoundary(board, x, y)){
-            return false;
-        }
-
-        if(!isAdjacentGrid(worker, x, y)){
-            return false;
-        }
-
-        Grid grid = board.getGrid(x, y);
-        if(grid.getOccupy()){
-            return false;
-        }
-        if(grid.getHeight() >= MAX_HEIGHT){
-            return false;
-        }
-        return true;
-    }
-
-    /**
-    * Check whether the game has a winner
-    * @param board - game board that has boundary
-    * @param worker - the worker that player chooses this turn
-    * @return boolean that demonstrates whether the game has a winner
-    */
-    public boolean isWin(Board board, Worker worker) {
-        int workerX = worker.getX();
-        int workerY = worker.getY();
-
-        Grid grid = board.getGrid(workerX, workerY);
-
-        if(grid.getHeight() == MAX_HEIGHT - 1) {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    
 }
